cd7ea92097f62d9c318cc8ef00179f354f6c5963
Gery Wahyu <gerywahyunugraha@gmail.com>
2020-04-17 19:17:30 +0800

    Modify api initializer

--- testAPIs.sh
+++ testAPIs.sh
@@ -177,13 +177,12 @@ curl -s -X POST \
   -d "{
 	\"chaincodeName\":\"dewallet\",
 	\"chaincodeVersion\":\"v0\",
-	\"chaincodeType\": \"$LANGUAGE\",
-	\"args\":[\"a\",\"100\",\"b\",\"200\"]
+	\"chaincodeType\": \"$LANGUAGE\"
 }"
 echo
 echo
 
-echo "POST invoke chaincode on peers of Org1 and Org2"
+echo "POST invoke Register on chaincode on peers of Org1 and Org2"
 echo
 VALUES=$(curl -s -X POST \
   http://localhost:4000/channels/mychannel/chaincodes/dewallet \
@@ -191,8 +190,13 @@ VALUES=$(curl -s -X POST \
   -H "content-type: application/json" \
   -d "{
   \"peers\": [\"peer0.org1.example.com\",\"peer0.org2.example.com\"],
-  \"fcn\":\"move\",
-  \"args\":[\"a\",\"b\",\"10\"]
+  \"fcn\":\"Register\",
+  \"args\":[\"{
+    \"username\": \"test\",
+    \"publicKey\": \"test\",
+    \"data\": \"test\",
+    \"verified\": \"false\"
+  }\"]
 }")
 echo $VALUES
 # Assign previous invoke transaction id  to TRX_ID
@@ -200,81 +204,23 @@ MESSAGE=$(echo $VALUES | jq -r ".message")
 TRX_ID=${MESSAGE#*ID: }
 echo
 
-echo "GET query chaincode on peer1 of Org1"
+echo "POST invoke GetPublicKey on chaincode on peers of Org1 and Org2"
 echo
-curl -s -X GET \
-  "http://localhost:4000/channels/mychannel/chaincodes/dewallet?peer=peer0.org1.example.com&fcn=query&args=%5B%22a%22%5D" \
+VALUES=$(curl -s -X POST \
+  http://localhost:4000/channels/mychannel/chaincodes/dewallet \
   -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
-echo
-echo
-
-echo "GET query Block by blockNumber"
-echo
-BLOCK_INFO=$(curl -s -X GET \
-  "http://localhost:4000/channels/mychannel/blocks/1?peer=peer0.org1.example.com" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json")
-echo $BLOCK_INFO
-# Assign previous block hash to HASH
-HASH=$(echo $BLOCK_INFO | jq -r ".header.previous_hash")
-echo
-
-echo "GET query Transaction by TransactionID"
-echo
-curl -s -X GET http://localhost:4000/channels/mychannel/transactions/$TRX_ID?peer=peer0.org1.example.com \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
-echo
-echo
-
-
-echo "GET query Block by Hash - Hash is $HASH"
-echo
-curl -s -X GET \
-  "http://localhost:4000/channels/mychannel/blocks?hash=$HASH&peer=peer0.org1.example.com" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "cache-control: no-cache" \
   -H "content-type: application/json" \
-  -H "x-access-token: $ORG1_TOKEN"
+  -d "{
+  \"peers\": [\"peer0.org1.example.com\",\"peer0.org2.example.com\"],
+  \"fcn\":\"GetPublicKey\",
+  \"args\":[\"{
+    \"username\": \"test\",
+  }\"]
+}")
+echo $VALUES
+# Assign previous invoke transaction id  to TRX_ID
+MESSAGE=$(echo $VALUES | jq -r ".message")
+TRX_ID=${MESSAGE#*ID: }
 echo
-echo
-
-echo "GET query ChainInfo"
-echo
-curl -s -X GET \
-  "http://localhost:4000/channels/mychannel?peer=peer0.org1.example.com" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
-echo
-echo
-
-echo "GET query Installed chaincodes"
-echo
-curl -s -X GET \
-  "http://localhost:4000/chaincodes?peer=peer0.org1.example.com" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
-echo
-echo
-
-echo "GET query Instantiated chaincodes"
-echo
-curl -s -X GET \
-  "http://localhost:4000/channels/mychannel/chaincodes?peer=peer0.org1.example.com" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
-echo
-echo
-
-echo "GET query Channels"
-echo
-curl -s -X GET \
-  "http://localhost:4000/channels?peer=peer0.org1.example.com" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
-echo
-echo
-
 
 echo "Total execution time : $(($(date +%s)-starttime)) secs ..."
